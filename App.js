e946e3375e4170a22ccd9adbde84cf049c2d4703
caueguedes <caueguedes@univem.edu.br>
2018-07-12 13:45:17 -0300

    initial

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React, { Component} from 'react';
 import {
     ActivityIndicator,
     AsyncStorage,
-    Button,
+    Keyboard,
     StatusBar,
     StyleSheet,
-    Text,
     TextInput,
-    View
+    TouchableWithoutFeedback,
+    View,
 } from 'react-native';
 
 import {
@@ -16,17 +16,33 @@ import {
 } from 'react-navigation';
 
 import {
+    Button ,
     ButtonGroup,
+    Card,
     Divider,
+    FormInput,
+    FormLabel,
+    FormValidationMessage,
     Header,
+    Icon,
+    List,
     ListItem,
-    SearchBar
+    SearchBar,
+    Text,
 } from 'react-native-elements'
 
 import ActionButton from 'react-native-action-button';
-import Icon from 'react-native-vector-icons/Ionicons';
+// import Icon from 'react-native-vector-icons/Ionicons';
 
+const DismissKeyboard = ({ children }) => (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        {children}
+    </TouchableWithoutFeedback>
+);
 
+var base64 = require('base-64');
+
+// ---------------- AuthLoadingScreen --------------------------------------
 class AuthLoadingScreen extends Component {
   constructor(props) {
     super(props);
@@ -50,6 +66,7 @@ class AuthLoadingScreen extends Component {
   }
 }
 
+// ---------------- SignInScreen --------------------------------------
 class SignInScreen extends Component {
     constructor(props){
         super(props);
@@ -58,34 +75,37 @@ class SignInScreen extends Component {
             pass: '',
         }
     }
-  // static navigationOptions = {
-  //       // title: 'Please sign in',
-  // };
+
+    _signInAsync = async () => {
+    // alert(this.obj.user.text + ' ' + this.obj.pass.text)
+    await AsyncStorage.setItem('userToken', 'abc');
+    this.props.navigation.navigate('App');
+  };
   render() {
     return (
+        <DismissKeyboard>
          <View style={styles.container_login}>
+            <Text h1 style={styles.logo}>Jurvalid</Text>
+            <FormLabel>Usuario</FormLabel>
+            <FormInput
+                placeholder="Digite seu Usuario"
+                onChangeText={(text) => this.obj.user = {text}}/>
 
-            <Text style={styles.logo}>
-            Jurvalid
-            </Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Digite seu email"
-              onChangeText={(text) => this.obj.user = {text}}
-            />
-             <TextInput
-              style={styles.input}
-              placeholder="Senha"
-              onChangeText={(text) => this.obj.pass = {text}}
-            />
-            <View style={styles.buttonContainer}>
+            <FormLabel>Senha</FormLabel>
+            <FormInput
+                password={true}
+                secureTextEntry={true}
+                placeholder="Senha"
+                onChangeText={(text) => this.obj.pass = {text}}/>
 
-                <Button style={{height: 50, width: 250}}
-                    onPress={this._signInAsync}
-                    title="Entrar"
-                  />
-            </View>
-      </View>
+            <Button
+                title='Entrar'
+                rounded={true}
+                    onPress={() => {this._signInAsync()}}
+            />
+            {/*<FormValidationMessage>Usuario ou senha com erro.</FormValidationMessage>*/}
+         </View>
+        </DismissKeyboard>
     );
   }
 
@@ -96,65 +116,105 @@ class SignInScreen extends Component {
   };
 }
 
+// ---------------- HomeScreen --------------------------------------
 class HomeScreen extends Component {
     constructor(props){
         super(props);
+        this.state ={ isLoading: true}
         // this._fetchList();
     }
+
+  componentDidMount(){
+    return fetch('https://jurvalidation.herokuapp.com/api/consultas.json', {
+       method: 'GET',
+        headers: {
+           Accept: 'application/json',
+           'Content-Type': 'application/json',
+           Authorization : 'Basic ' +  base64.encode('staff' + ':' + 'staff123')
+        },
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+        this.setState({
+            isLoading: false,
+            dataSource: responseJson,
+        }, function(){
+            console.log(responseJson)
+        });
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  keyExtractor = (item, index) => index
+
+    renderItem = ({ item }) => (
+      <ListItem
+        title={item.name}
+        subtitle={item.subtitle}
+        leftAvatar={{
+          source: item.avatar_url && { uri: item.avatar_url },
+          title: item.name[0]
+        }}
+      />
+    )
 
     static navigationOptions =  {
         headerTitle: 'Consultas',
     };
     render() {
-        return (
-             <View style={{flex:1}}>
-                <SearchBar lightTheme
-                        onChangeText={this._onPressButton()}
-                        onClear={this._onPressButton()}
-                        placeholder='Buscar' />
-                {
-                    list.map((l, i) => (
-                      <ListItem
-                        key={i}
-                        leftAvatar={{ source: { uri: l.avatar_url } }}
-                        title={l.name}
-                        subtitle={l.subtitle}
-                        onPress={() => this.props.navigation.navigate('Detail')}
-
-                      />
-                    ))
-                }
-
-                <ActionButton
-                    buttonColor="rgba(231,76,60,1)"
-                    onPress={() => {this._showMoreApp()}}
-                    onLongPress={() => {this._signOutAsync()}}
-                />
+        if(this.state.isLoading){
+          return(
+            <View style={{flex: 1, padding: 20}}>
+              <ActivityIndicator/>
             </View>
-          // <View style={styles.container}>
-          //   <Button title="Show me more of the app" onPress={this._showMoreApp} />
-          //   <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
-          // </View>
+          )
+        }
+
+
+
+        return (
+            <DismissKeyboard>
+                 <View style={{flex:1}}>
+                    <SearchBar lightTheme
+                            onChangeText={this._onPressButton()}
+                            onClear={this._onPressButton()}
+                            placeholder='Buscar' />
+
+                    {
+                        this.state.dataSource.map((l, i) => (
+                            <ListItem
+                            key={i}
+                            // leftAvatar={{ source: { uri: l.avatar_url } }}
+                            title={l.nomeParte}
+                            subtitle={l.docParte}
+                            onPress={() => this._redirectOpt(l.tj_status, l.trf_status, l.trt_status, l.id)}
+                            rightIcon={{ uri: 'app/images/green.png'}}
+                            hideChevron
+
+                          />
+                        ))
+                    }
+                    <ActionButton
+                        buttonColor="rgba(231,76,60,1)"
+                        onPress={() => {this._showMoreApp()}}
+                        onLongPress={() => {this._signOutAsync()}}
+                    />
+                </View>
+            </DismissKeyboard>
         );
     }
-    _onPressButton = () => {
+    _redirectOpt = (var1, var2, var3, id) => {
+        if(var1 !== 1 | var2 !== 0 | var3 !== 1){
+            this.props.navigation.navigate('Detail',{
+                consultaId: id
+            });
+        }
     }
 
-     _fetchList = () => {
-        alert('oi');
-        fetch('http://127.0.0.1:8000/api/list', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                // 'Authorization': 'Basic '+  this.props.obj.user + ':' + this.props.obj.pass,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                firstParam: 'yourValue',
-                secondParam: 'yourOtherValue',
-            }),
-        });
-     }
+    _onPressButton = () => {
+    }
 
     _showMoreApp = () => {
         this.props.navigation.navigate('Busca');
@@ -165,128 +225,229 @@ class HomeScreen extends Component {
         this.props.navigation.navigate('Auth');
     };
 }
-//Excluir
-const list = [
-  {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  },
-]
 
-const listPendencias = [
-  {
-    name: 'TjSP - 11111111111111-1',
-    subtitle: 'Ação civil - Prefeitura'
-  },
-  {
-    name: 'TjSP - 22222222222222-2',
-    subtitle: 'Testo 2'
-  },
-  {
-      name: 'TjSP - 33333333333333-3',
-    subtitle: 'Testo 3'
-  },
-  {
-    name: 'TjSp - 44444444444444-4',
-    subtitle: 'Testo 4'
-  },
-]
-
+// ---------------- BuscaScreen --------------------------------------
 class BuscaScreen extends Component {
-  static navigationOptions = {
+    constructor(props){
+        super(props);
+        this.state ={
+            isLoading: false,
+            name: '',
+            doc: '',
+        }
+    }
+
+    consulta(){
+
+    this.setState({isLoading: true});
+    return fetch('https://jurvalidation.herokuapp.com/api/consultas.json', {
+       method: 'POST',
+        headers: {
+           Accept: 'application/json',
+           'Content-Type': 'application/json',
+           Authorization : 'Basic ' +  base64.encode('staff' + ':' + 'staff123')
+        },
+        body: JSON.stringify({
+            name: "Caue Vieira Guedes",
+            doc: 40423362801,
+            // name: this.state.name,
+            // doc: this.state.doc,
+        }),
+    })
+    .then((response) => response.status)
+    .then((responseJson) => {
+        this.setState({
+            isLoading: false,
+            dataSource: responseJson,
+        }, function(){
+            console.log(responseJson);
+            // alert('Consulta Realizada');
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+    static navigationOptions = {
     title: 'Realizar Consultas',
   };
     render(){
-        return (
-             <View >
-                <TextInput
-                  style={styles.input}
-                  placeholder="Nome para Busca"
-                  // onChangeText={(text) => this.setState({text})}
-                />
-                 <TextInput
-                  style={styles.input}
-                  placeholder="Cpf para Busca"
-                  // onChangeText={(text) => this.setState({text})}
-                />
-                <View style={styles.buttonContainer}>
-
-                    <Button style={{height: 50, width: 250}}
-                        onPress={() => {alert('Consulta Realizada')}}
-                        title="Realizar Consulta"
-                      />
-                </View>
+         if(this.state.isLoading){
+          return(
+            <View style={{flex: 1, padding: 20}}>
+              <ActivityIndicator/>
             </View>
+          )
+        }
+
+        return (
+
+             <DismissKeyboard>
+
+                 <View >
+
+                    <FormLabel>Nome:</FormLabel>
+
+                     <FormInput style={{paddingHorizontal:10}}
+                                onChangeText={(text) => this.setState({ doc: text}) }
+                                placeholder="Digite nome para Busca"/>
+                     <Divider style={{backgroundColor: '#d3d3d3', marginHorizontal:12, paddingTop:-10}}/>
+
+                    <FormLabel>CPF/CNPJ:</FormLabel>
+                    <FormInput style={{marginLeft:14, marginBottom:1}}
+                               onChangeText={(text) => this.setState({ doc: text}) }
+                               placeholder="Digite nome para Busca"/>
+                     <Divider style={{backgroundColor: '#d3d3d3', marginHorizontal:12, paddingTop:-10}}/>
+
+                     <Button style={{marginTop:40}}
+                             title='Realizar Consulta'
+                             onPress={()=> {this.consulta().then(() => this.props.navigation.navigate('Home'))}}/>
+                             {/*onPress={()=> {this.consulta(this.state.name, this.state.doc)}}/>*/}
+
+                {/*<FormValidationMessage>Error message</FormValidationMessage>*/}
+
+                {/*<TextInput*/}
+                  {/*style={styles.input}*/}
+                  {/*placeholder="Nome para Busca"*/}
+                  {/*// onChangeText={(text) => this.setState({text})}*/}
+                {/*/>*/}
+                 {/*<TextInput*/}
+                  {/*style={styles.input}*/}
+                  {/*placeholder="Cpf para Busca"*/}
+                  {/*// onChangeText={(text) => this.setState({text})}*/}
+                {/*/>*/}
+                {/*<View style={styles.buttonContainer}>*/}
+
+                    {/*<Button style={{height: 50, width: 250}}*/}
+                        {/*onPress={() => {alert('Consulta Realizada')}}*/}
+                        {/*title="Realizar Consulta"*/}
+                      {/*/>*/}
+                {/*</View>*/}
+                </View>
+             </DismissKeyboard>
         );
     }
 }
 
+// ---------------- DetailsScreen --------------------------------------
 class DetailsScreen extends Component {
-    constructor () {
-        super()
+    constructor (props) {
+        super(props)
         this.state = {
-            selectedIndex: 1
+            isLoading: true,
         }
-    this.updateIndex = this.updateIndex.bind(this)
+        this.componentDidMountDetail()
+
     }
 
-    updateIndex (selectedIndex) {
-        this.setState({selectedIndex})
+  componentDidMountDetail(){
+        const { navigation } = this.props;
+        return fetch('https://jurvalidation.herokuapp.com/api/detalhes.json', {
+            method: 'POST',
+            headers: {
+               Accept: 'application/json',
+               'Content-Type': 'application/json',
+               Authorization : 'Basic ' +  base64.encode('staff' + ':' + 'staff123')
+            },
+            body: JSON.stringify({
+                id_consulta: navigation.getParam('consultaId', 'NO-ID'),
+            }),
+        })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            this.setState({
+                isLoading: false,
+                dataSourceDetail: responseJson,
+            }, function(){
+                console.log(responseJson)
+            });
+          })
+          .catch((error) =>{
+            console.error(error);
+          });
+    }
+
+    _status(){
+      if(this.state.dataSourceDetail.tj_status === 1 &&
+          this.state.dataSourceDetail.trt_status === 1 &&
+              this.state.dataSourceDetail.trf_status === 1){
+
+          return 'Liberado';
+
+      }else if(this.state.dataSourceDetail.tj_status === 3 |
+          this.state.dataSourceDetail.trt_status === 3 |
+              this.state.dataSourceDetail.trf_status === 3){
+
+            return 'Proibido';
+
+      // }else if(this.state.dataSourceDetail.tj_status === 0 |
+      //     this.state.dataSourceDetail.trt_status === 0 |
+      //         this.state.dataSourceDetail.trf_status === 0){
+      //
+      //       return 'Forçar Atualização';
+      }else if(this.state.dataSourceDetail.tj_status === 2 |
+          this.state.dataSourceDetail.trt_status === 2 |
+              this.state.dataSourceDetail.trf_status === 2){
+
+            return 'Averiguação Pendências';}
+        }
+
+
+    _comentario(){
+        if(this.state.dataSourceDetail.comentario.length === 0 ){
+            return 'Nenhum comentario até o momento'
+        }else{
+            return this.state.dataSourceDetail.comentario.text
+        }
     }
 
     static navigationOptions = {
-    title: 'Detalhe da Consulta',
-  };
-    render(){
-        const buttons = ['Liberado', 'Averiguação', 'Pendencias']
-        const { selectedIndex } = this.state
+      title: 'Detalhe da Consulta',
+    };
+
+  render(){
+         if(this.state.isLoading){
+            return(
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        }
         return (
             <View>
                 {/*<Header centerComponent={{ text: 'Detalhes', style: { color: '#fff' } }}/>*/}
-                <Text h4 style={{textAlign: 'center'}}>Status da Consulta:</Text>
+                <Text style={{paddingTop:20, paddingHorizontal: 18}}>
+                    <Text style={{ fontSize: 16, fontWeight: 'bold',}} >Status da Consulta:</Text>
+                    <Text style={{fontSize: 16}}> {this._status()}</Text>
+                </Text>
 
+                <Text style={styles.textDetail}>
+                    <Text style={{ fontSize: 16, fontWeight: 'bold',}} >Nome:</Text>
+                    <Text style={{fontSize: 16}}> {String(this.state.dataSourceDetail.nomeParte).substring(0,40)}</Text>
+                </Text>
 
-                <ButtonGroup
-                  onPress={this.updateIndex}
-                  selectedIndex={selectedIndex}
-                  buttons={buttons}
-                  containerStyle={{height: 40}}
-                />
+                <Text style={styles.textDetail}>
+                    <Text style={{ fontSize: 16, fontWeight: 'bold',}} >Cpf/Cnpj:</Text>
+                    <Text style={{fontSize: 16}}> {this.state.dataSourceDetail.docParte}</Text>
+                </Text>
 
+                <Card>
+                    <Text style={{textAlign:'left', fontWeight: 'bold', }}>
+                        Comentarios:
+                    </Text>
+                    <Divider style={{backgroundColor: '#d3d3d3', marginVertical: 6 }}/>
+                    <Text style={{color:'#d3d3d3'}}>
+                        {this._comentario()}
+                    </Text>
 
-
-                {
-                    listPendencias.map((l, i) => (
-                        <ListItem
-                        key={i}
-                        leftAvatar={{ source: { uri: l.avatar_url } }}
-                        title={l.name}
-                        subtitle={l.subtitle}
-                        />
-                    ))
-                }
-            </View>
+                </Card>
+                </View>
         );
     }
 }
-
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Open up App.js to start working on your app!</Text>
-//         <Text>Changes you make will automatically reload.</Text>
-//         <Text>Shake your phone to open the developer menu.</Text>
-//       </View>
-//     );
-//   }
-// }
 
 const AppStack = createStackNavigator({
     Home: HomeScreen,
@@ -301,6 +462,11 @@ const AppStack = createStackNavigator({
 
 const AuthStack = createStackNavigator({
     SignIn: SignInScreen},
+    {
+    headerMode: 'none',
+    navigationOptions: {
+        headerVisible: false,}
+    }
     );
 
 export default createSwitchNavigator(
@@ -339,6 +505,8 @@ const styles = StyleSheet.create({
     fontSize: 42,
     textAlign: 'center',
   },
+  textDetail: {
+    paddingHorizontal: 18,
+    paddingVertical:2
+  }
 });
-
-
